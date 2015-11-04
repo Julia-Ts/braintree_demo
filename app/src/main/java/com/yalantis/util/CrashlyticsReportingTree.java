bdb9072fced2ab57157d380205329abb37f7cbe7
Artem Kholodnyi <artem.kholodnyi@yalantis.com>
2015-11-04 18:09:38 +0200

    Add timber reporting via crashlytics

--- app/src/main/java/com/yalantis/util/CrashlyticsReportingTree.java
+++ app/src/main/java/com/yalantis/util/CrashlyticsReportingTree.java
@@ -1,7 +1,28 @@
 package com.yalantis.util;
 
+import android.util.Log;
+
+import com.crashlytics.android.Crashlytics;
+
+import timber.log.Timber;
+
 /**
  * Created by Artem Kholodnyi on 11/4/15.
  */
-public class CrashlyticsReportingTree {
+
+public class CrashlyticsReportingTree extends Timber.Tree {
+    public final static int MIN_LOG_PRIORITY = Log.DEBUG;
+
+    @Override
+    public void log(int priority, String tag, String message, Throwable t) {
+        if (priority < MIN_LOG_PRIORITY) {
+            return;
+        }
+
+        Crashlytics.log(priority, tag, message);
+
+        if (t != null) {
+            Crashlytics.logException(t);
+        }
+    }
 }
