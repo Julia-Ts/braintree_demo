746eb9753f1d327ade99098143c9c875dcba980f
Dmitriy Dovbnya <dmitriy.dovbnya@yalantis.com>
2016-06-28 11:27:29 +0300

    new template

--- app/src/main/java/com/yalantis/manager/ApiManager.java
+++ app/src/main/java/com/yalantis/manager/ApiManager.java
@@ -4,28 +4,14 @@ import android.content.Context;
 
 import com.google.gson.GsonBuilder;
 import com.yalantis.api.ApiSettings;
-import com.yalantis.api.LogOutWhenSessionExpired;
-import com.yalantis.api.body.ErrorBody;
-import com.yalantis.api.body.ResultBody;
 import com.yalantis.api.deserializer.StringDeserializer;
 import com.yalantis.api.services.NetworkService;
 import com.yalantis.interfaces.Manager;
-import com.yalantis.model.SomeModel;
 
-import java.util.Random;
-
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
-import okhttp3.ResponseBody;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.HttpException;
 import retrofit2.converter.gson.GsonConverterFactory;
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func1;
-import rx.schedulers.Schedulers;
 
 public class ApiManager implements Manager {
 
@@ -68,21 +54,11 @@ public class ApiManager implements Manager {
     }
 
     private void initServices(Retrofit retrofit) {
-        mNetworkService = retrofit.create(NetworkService.class);
         //TODO: init services
     }
 
-    public Observable<SomeModel> someApiCall() {
-        return mNetworkService.someApiCall()
-                .subscribeOn(Schedulers.io())
-                .map(new Func1<ResultBody<SomeModel>, SomeModel>() {
-                    @Override
-                    public SomeModel call(ResultBody<SomeModel> body) {
-                        return body.getResult();
-                    }
-                })
-                .retryWhen(new LogOutWhenSessionExpired())
-                .observeOn(AndroidSchedulers.mainThread());
-    }
+    /**
+     * Api requests
+     */
 
 }
--- app/src/main/java/com/yalantis/model/SomeModel.java
+++
@@ -1,7 +0,0 @@
-package com.yalantis.model;
-
-/**
- * Created by voltazor on 27/06/16.
- */
-public class SomeModel {
-}
